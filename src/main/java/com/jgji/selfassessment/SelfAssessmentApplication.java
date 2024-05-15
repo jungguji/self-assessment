4d94431ed91aa93a78e99db5c8fb1dd5f2b8c68e
JG.JI <junggu.ji.dev@gmail.com>
2024-05-15 19:54:01 +0900

    project init

---
+++ src/main/java/com/jgji/selfassessment/SelfAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.jgji.selfassessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelfAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelfAssessmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jgji/selfassessment/SelfAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jgji.selfassessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelfAssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
