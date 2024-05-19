b205eb099e689a25ad186d4279c2f70221c99e3c
JG.JI <junggu.ji.dev@gmail.com>
2024-05-19 19:39:19 +0900

    feat: 일단위 평가 클래스 생성

---
+++ src/main/java/com/jgji/selfassessment/domain/evaluation/DailyEvaluation.java
@@ -0,0 +1,40 @@
+package com.jgji.selfassessment.domain.evaluation;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Comment;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDate;
+
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(
+        name = "daily_evaluations",
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = {"user_id", "date"})
+        }
+)
+public class DailyEvaluation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false, updatable = false)
+    private long id;
+
+    @Comment("유저 아이디")
+    @Column(name = "user_id", nullable = false)
+    private long userId;
+
+    @Comment("평가 날짜")
+    @Column(name = "date", nullable = false)
+    private LocalDate date;
+
+    @Comment("점수")
+    @Column(name = "score", nullable = false)
+    private float score = 0.0f;
+}
