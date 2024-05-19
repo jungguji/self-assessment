928ae26edae82e0c01f956d76221d61c6320ff62
JG.JI <junggu.ji.dev@gmail.com>
2024-05-19 19:27:48 +0900

    feat: 평가 테이블 추가

---
+++ src/main/java/com/jgji/selfassessment/domain/evaluation/Evaluation.java
@@ -0,0 +1,40 @@
+package com.jgji.selfassessment.domain.evaluation;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Comment;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "evaluations")
+public class Evaluation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false, updatable = false)
+    private long id;
+
+    @Comment("활동 ID")
+    @Column(name = "activity_id", nullable = false)
+    private long activityId;
+
+    @Comment("평점")
+    @Column(name = "score", nullable = false)
+    private int score;
+
+    @Comment("코멘트")
+    @Column(name = "comment", columnDefinition = "TEXT")
+    private String comment;
+}
