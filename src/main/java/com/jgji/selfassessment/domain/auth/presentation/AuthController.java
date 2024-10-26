8fc6889e181e193461ac6e09c8b4c4fc37b3619b
JG.JI <junggu.ji.dev@gmail.com>
2024-10-26 22:38:41 +0900

    refactor: 서비스레이어로 이동

--- src/main/java/com/jgji/selfassessment/domain/auth/presentation/AuthController.java
+++ src/main/java/com/jgji/selfassessment/domain/auth/presentation/AuthController.java
@@ -1,20 +1,16 @@
 package com.jgji.selfassessment.domain.auth.presentation;
 
+import com.jgji.selfassessment.domain.auth.application.AuthenticationService;
 import com.jgji.selfassessment.domain.auth.presentation.dto.SignInRequest;
 import com.jgji.selfassessment.domain.auth.presentation.dto.SignInResponse;
 import com.jgji.selfassessment.global.config.security.token.TokenProvider;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -22,17 +18,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AuthController {
 
-    private final AuthenticationManager authenticationManager;
     private final TokenProvider tokenProvider;
+    private final AuthenticationService authenticationService;
 
     @ResponseStatus(HttpStatus.OK)
     @PostMapping("/sign-in")
     public SignInResponse login(@RequestBody SignInRequest signInRequest) {
-        Authentication authentication = authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(
-                        signInRequest.email(),
-                        signInRequest.password()
-                )
+        Authentication authentication = authenticationService.getAuthenticate(
+                signInRequest.email(),
+                signInRequest.password()
         );
 
         SecurityContextHolder.getContext().setAuthentication(authentication);
